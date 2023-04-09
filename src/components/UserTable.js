d63728b8c1121dddc4a2298c080608dadb43ff4b
Kiran Sahu <61940769+kiransahu-dev@users.noreply.github.com>
2023-04-09 09:14:45 +0530

    commit for testcases

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,5 +1,40 @@
 import React, { useState, useEffect } from "react";
 
-function UserTable() {}
+function UserTable() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data.sort((a, b) => a.id - b.id));
+      });
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Website</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.phone}</td>
+            <td>{user.website}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 export default UserTable;
